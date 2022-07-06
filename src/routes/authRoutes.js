26b705de804576319de0253992073c8b5404afe3
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-06 19:43:40 -0300

    feat: criação da rota POSTt/sign-in

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { createUser } from "../controllers/authControllers.js";
+import { createUser, loginUser } from "../controllers/authControllers.js";
 
 const router = Router();
 
 router.post("/sign-up", createUser);
+router.post("/sign-in", loginUser);
 
 export default router;
