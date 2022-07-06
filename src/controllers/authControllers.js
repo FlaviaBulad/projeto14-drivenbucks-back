7e904514755dd2afd6bfc12dcd58f4f5b0aa355c
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-06 16:29:42 -0300

    test: database connection is working

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,8 +1,16 @@
 import { db } from "../database/mongodb.js";
-import { authSignUpSchema } from "../schemas/authSchemas.js";
+import authSignUpSchema from "../schemas/authSchemas.js";
 
 import bcrypt from "bcrypt";
 
 export async function CreateUser(req, res) {
   const newUser = req.body;
+
+  await db.collection("users").insertOne({
+    name: newUser.name,
+    email: newUser.email,
+    password: newUser.password,
+  });
+
+  res.status(200).send("Cadastrou");
 }
