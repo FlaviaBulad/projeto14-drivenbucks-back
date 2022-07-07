7cff88ac82d5dee4c75899601a2c3d0f4bb49ff2
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-06 21:30:08 -0300

    feat(authControllers): Adicionei um if na function createUser para ver se já existe um usuário criado com o email no banco de dados

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -2,6 +2,7 @@ import { db } from "../database/mongodb.js";
 import {authSignUpSchema, authSignInSchema} from "../schemas/authSchemas.js";
 
 import bcrypt from "bcrypt";
+import router from "../routes/authRoutes.js";
 
 export async function createUser(req, res) {
   try {
@@ -12,19 +13,25 @@ export async function createUser(req, res) {
     if (validate.error) {
       return res.status(422).send("Reveja os campos");
     }
+    
+    const emailAlreadyRegistered = await db.collection("users").findOne({email: newUser.email});
 
+    if(emailAlreadyRegistered){
+      return res.status(422).send("Email já cadastrado")
+    };
+    
     const SALT = 10;
     const passwordHash = bcrypt.hashSync(newUser.password, SALT);
 
     await db.collection("users").insertOne({
       name: newUser.name,
       email: newUser.email,
-      password: passwordHash,
+      password: passwordHash
     });
 
     res.status(200).send("Usuário cadastrado com sucesso");
   } catch (error) {
-    console.error("Houve um problema ao tentar cadastrar o usuário");
+    console.error(error);
     res.status(500).send("Houve um problema ao tentar cadastrar o usuário");
   }
 }
@@ -41,18 +48,18 @@ export async function loginUser(req, res){
     const registeredUser = await db.collection("users").findOne({email: user.email});
 
     if(!registeredUser){
-      res.status(422).send("Email ou senha inválidos")
+      return res.status(422).send("Email ou senha inválidos")
     };
 
     const correctPassword = bcrypt.compareSync(user.password, registeredUser.password);
     if(!correctPassword){
-      res.status(422).send("Email ou senha inválidos")
+      return res.status(422).send("Email ou senha inválidos")
     };
 
     const token = uuid();
     await db.collection('sessions').insertOne({ token, userId: registeredUser._id });
 
-    return res.status(200).send({ token });
+    res.status(200).send({ token });
   } catch (error) {
     console.log(error);
     res.status(500).send('Houve um problema ao logar o usuário');
