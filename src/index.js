db33914600525cc4f3ea16c9ba15181f974eda4a
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-09 08:08:05 -0300

    feat: Retirada do import do db do arquivo index.js, não era utilizado ali.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import cors from "cors";
-import { db } from "./database/mongodb.js";
+
 
 import authRoutes from "./routes/authRoutes.js";
 
