a757135d781d75360d84f81a13d4173b7c178a25
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-10 10:22:20 -0300

    feat: Finalizado a rota produtos

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -9,7 +9,11 @@ export async function getProducts(req, res){
     if(!session){
         return res.status(404).send("token não existe!");
     };
+    
+    const products = await db.collection("products").find({}).toArray();
+    
     res.status(200).send(products);
+    
     } catch (error) {
         console.error(error);
         res.status(500).send("Houve um problema ao tentar buscar os produtos");
